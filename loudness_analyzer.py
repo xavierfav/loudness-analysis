63fdc436a826101f2367373734d4f5a1a5baf850
xavierfav <xavier.favory@gmail.com>
2018-05-31 14:07:28 +0200

    Add watchdog timer for analyzer

--- loudness_analyzer.py
+++ loudness_analyzer.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 import json
 import time
+import signal
+import errno
+from functools import wraps
 from datetime import datetime
 import progress_bar
 import tempfile
@@ -20,6 +23,40 @@ def log(s):
     sys.stdout.write(str(s) + "\n")
     sys.stdout.flush()
 
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    """
+    Decorator for raising exception after a method takes to long to compute.
+    Some methods fail and get stuck...
+    """
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+@timeout(5*60)
+def analyze(cmd, tmp):
+    # call Essentia's MusicExtractor
+    process = subprocess.Popen(cmd.split(), stdout=DEVNULL, stderr=DEVNULL)
+    process.communicate()
+    # load computed descriptors
+    data = json.load(tmp)
+    return data
+           
 def extract(list_filenames, job_id):
     output_dict = dict()
     files_not_analyzed = list()
@@ -46,11 +83,8 @@ def extract(list_filenames, job_id):
         tmp = tempfile.NamedTemporaryFile()
         cmd = CMD_DIR + COMMAND.format(audiofile, tmp.name)
         try:
-            # call Essentia's MusicExtractor
-            process = subprocess.Popen(cmd.split(), stdout=DEVNULL, stderr=DEVNULL)
-            process.communicate()
-            # load computed descriptors
-            data = json.load(tmp)
+            data = analyze(cmd, tmp)
+
             output_dict[fs_id]["ebur128"] = data["lowlevel"]["loudness_ebu128"]["integrated"]
             output_dict[fs_id]["replayGain"] = data["metadata"]["audio_properties"]["replay_gain"]
             #log("{} analyzed succesfully".format(filename))
@@ -90,3 +124,4 @@ if __name__ == "__main__":
     files = ["095_456.mp3", "123_123.wav", "456_123.mp3", "789_456.mp3"]
     root = "sounds/0/"
     extract([root + f for f in files], 0)
+
